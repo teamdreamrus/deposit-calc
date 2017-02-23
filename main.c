977252aa1ae7add8f61ee4fb37a8875ebe47c01d
teamdreamrus <atazhanov.biz@gmail.com>
2017-02-23 20:20:06 +0700

    create function proffit

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+int proffit(int x,int z);
+
 int main()
 {
     int s,t;
@@ -9,9 +11,36 @@ int main()
     scanf("%i",&s);
     if ((t <= 0) || (t > 365) || (s<10000))
     printf("enter correct data! \n");
-    else{
+    else
+	{
         printf("so good \n");
     }
     return 0;
 }
 
+int proffit(int x,int z){
+	int cashback=x;
+	if(x<100000)
+	{
+	    if ((z > 0) && (z <= 30)) 
+        cashback*=0.9;
+        if ((z > 30) && (z <= 120)) 
+        cashback*=1.02;
+        if ((z > 120) && (z <= 240)) 
+        cashback*=1.06;
+         if ((z > 240) && (z <= 365)) 
+        cashback*=1.12;
+    }
+	else
+	{
+		if ((z > 0) && (z <= 30)) 
+        cashback*=0.9;
+        if ((z > 30) && (z <= 120)) 
+        cashback*=1.03;
+        if ((z > 120) && (z <= 240)) 
+        cashback*=1.08;
+         if ((z > 240) && (z <= 365)) 
+        cashback*=1.15;
+	}
+return cashback;
+}
