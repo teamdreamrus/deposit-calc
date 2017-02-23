7cac732905b0964c22b7fb209e3fad141adf9d16
teamdreamrus <atazhanov.biz@gmail.com>
2017-02-23 20:09:32 +0700

    create input calidation for correctness

--- main.c
+++ main.c
@@ -7,8 +7,11 @@ int main()
     scanf("%i",&t);
     printf("enter sum to deposit: \n");
     scanf("%i",&s);
-    printf("hello world! \n");
-    
+    if ((t <= 0) || (t > 365) || (s<10000))
+    printf("enter correct data! \n");
+    else{
+        printf("so good \n");
+    }
     return 0;
 }
 
