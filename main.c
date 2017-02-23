1b249d2401f85380809f0e072d74469dca7d12b8
teamdreamrus <atazhanov.biz@gmail.com>
2017-02-23 19:22:37 +0700

    delete fuction proffit

--- main.c
+++ main.c
@@ -11,20 +11,10 @@ int main()
 	printf("Write time contribution days (1-365):\n ");
 	scanf("%f \n",t);
 	if  ((s>=1000) && (t<=365) && (t>0)) 
-	   {
-	printf("%f",Proffit(s,t));
-		}
+	  
 	else printf("Write true data \n");
 	system("pause");
 	return 0;
 	
 }
-int Proffit(int x,int z)
-{
-	int prof; 
-	if (z<=30) prof=x*0.9;
-	if ((z>30)&&(z<=120)) prof=x*1.02;
-	if ((z>120)&&(z<=240)) prof=x*1.06;
-	if ((z>240)&&(z<=365)) prof=x*1.12;
-    return prof;
-}
+
