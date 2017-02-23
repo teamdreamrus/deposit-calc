8a5eabc96d30fb212b6fd95744fd0ed2d6757789
teamdreamrus <atazhanov.biz@gmail.com>
2017-02-23 19:00:47 +0700

    Change print

--- main.c
+++ main.c
@@ -8,9 +8,9 @@ int main()
 	int s,t;
 	printf("Write sum contribution (>1000):\n");
 	scanf("%f \n",s);	
-	printf("Write time contribution days (0-365):\n ");
+	printf("Write time contribution days (1-365):\n ");
 	scanf("%f \n",t);
-	if  ((s>=1000) && (t<=365)) {
+	if  ((s>=1000) && (t<=365) &&(t>0)) {
 		Proffit(s,t);
 		
 		}
