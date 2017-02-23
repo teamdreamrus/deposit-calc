38f75564d12c37c2969e87b962d450c0328e5abd
teamdreamrus <atazhanov.biz@gmail.com>
2017-02-23 19:24:50 +0700

    delete all without include

--- main.c
+++ main.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
 
 
-int Proffit(int x,int z);
+
 
 int main()
 {
-	int s,t;
-	printf("Write sum contribution (>1000):\n");
-	scanf("%f \n",s);	
-	printf("Write time contribution days (1-365):\n ");
-	scanf("%f \n",t);
-	if  ((s>=1000) && (t<=365) && (t>0)) 
-	  
-	else printf("Write true data \n");
+	
 	system("pause");
 	return 0;
 	
